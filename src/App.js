b25c3fe67a61dd3d56560b324953fe0c8e7249c7
Santosh.Bhat.Contractor <santosh.bhat.contractor@3dscom>
2021-08-13 10:34:41 +0530

    last changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CreateTypes from './pages/CreateTypes';
+import CreateStatus from './pages/CreateStatus';
+import RegisterTags from './pages/RegisterTags';
+import CreateResource from './pages/CreateResource';
+import AddTagValue from './pages/AddTagValue';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/types' component={CreateTypes} />
+          <Route path='/status' component={CreateStatus} />
+          <Route path='/tagReg' component={RegisterTags} />
+          <Route path='/resource' component={CreateResource} />
+          <Route path='/tagValue' component={AddTagValue} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
